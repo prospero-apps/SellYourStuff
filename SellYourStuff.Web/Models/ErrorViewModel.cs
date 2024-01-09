de3ab4e0941028285de22fdf5a8e7ed4f01f0569
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-01-09 13:44:13 +0100

    Add project files.

---
+++ SellYourStuff.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SellYourStuff.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
